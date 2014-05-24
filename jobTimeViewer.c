37caeac9ea22518ee79bdce32b8292e2f8ba3b4d
Alessio Balsini <a.balsini@sssup.it>
2014-05-24 22:20:54 +0200

    Added sources

---
+++ jobTimeViewer.c
@@ -0,0 +1,228 @@
+#include <linux/module.h>
+#include <linux/kprobes.h>
+
+#include <linux/kernel.h>
+
+#include <linux/pid.h>
+#include <linux/sched.h>
+
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+
+#include <linux/spinlock.h>
+
+#include <kernel/sched/sched.h>
+#include <fs/proc/internal.h>
+
+#define CONSOLE_LOG_LEVEL KERN_EMERG
+
+#define MODULE_NAME "jobTimeViewer"
+#define MODULE_NAME_PRINTK "jobTimeViewer : "
+#define SCHED_DL_PROC_FILENAME "deadline_stat"
+
+#define PROC_FS_FOLDER_NAME "sched_deadline"
+
+#include "utility.h"
+
+struct task_list_elem_t * head;
+
+static struct proc_dir_entry * sched_dl_folder;
+
+static int sched_dl_show(struct seq_file * m, void * v)
+{
+  struct task_list_elem_t * p;
+  unsigned int i, count;
+
+  p = (struct task_list_elem_t *)(m->private);
+
+  count = p->buffer.count;
+  i = p->buffer.head;
+
+  //seq_printf(m, "pid ( %d ) data# ( %d )\n", p->pid, count);
+  //seq_printf(m, "-----------------------\n");
+
+  while (count > 0) {
+    //seq_printf(m, "[%lu.%ld] %lld : %c\n",
+    //           (unsigned long)(p->buffer.time[i].tv_sec),
+    //           (long int)p->buffer.time[i].tv_nsec,
+    //           p->buffer.C[i],
+    //           p->buffer.miss[i]);
+    //seq_printf(m, "%lld\n", p->buffer.C[i]);
+    seq_printf(m, "%lu %ld %lld ; \n",
+               (unsigned long)(p->buffer.time[i].tv_sec),
+               (long int)p->buffer.time[i].tv_nsec,
+               p->buffer.C[i]);
+    i = (i + 1) % PERIODS_TO_REMEMBER;
+    --count;
+  }
+
+  return 0;
+}
+
+static int sched_dl_open(struct inode * inode, struct  file * file)
+{
+  int ret;
+
+  //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"A created file with inode: %p\n", inode);
+  //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"A created file with address: %p\n", file);
+  //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"file->private: %p\n", PDE_DATA(inode));
+
+  ret = single_open(file, sched_dl_show, PDE_DATA(inode));
+
+  //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"B created file with address: %p\n", file);
+
+  return ret;
+}
+
+/*
+ * A replenishment has been requested
+ */
+static void inst_replenish_dl_entity(struct sched_dl_entity *dl_se,
+                                     struct sched_dl_entity *pi_se)
+{
+  struct task_list_elem_t * p;
+  u64 exec_time;
+  u64 server_bandwidth_used;
+  char miss;
+
+  p = get_elem_by_task_dl_descriptor(head, (void *)dl_se);
+  if (p) {
+    exec_time = p->task_struct_pointer->se.sum_exec_runtime - p->buffer.last;
+    miss = exec_time >= dl_se->dl_deadline ? 'Y' : 'N';
+    server_bandwidth_used = dl_se->dl_deadline / exec_time;
+
+    //These two values are always the same!!
+    //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"last dl_se->runtime: %lld\n", dl_se->runtime);
+    //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"last pi_se->runtime: %lld\n", pi_se->runtime);
+
+    // Relative deadline
+    //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"last dl_se->dl_deadline: %lld\n", dl_se->dl_deadline);
+    //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"last pi_se->dl_deadline: %lld\n", pi_se->dl_deadline);
+
+    //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"last dl_se->dl_period: %lld\n", dl_se->dl_period);
+    //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"last pi_se->dl_period: %lld\n", pi_se->dl_period);
+
+    //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"last exec_time: %lld\n", exec_time);
+
+    //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"last server_bandwidth_used: %lld\n", server_bandwidth_used);
+
+
+    cb_enqueue(&(p->buffer),
+               exec_time,
+               miss,
+               current_kernel_time());
+/*
+
+    cb_enqueue(&(p->buffer),
+               pi_se->dl_runtime - dl_se->runtime,
+               miss,
+               current_kernel_time());
+*/
+    //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"last exec: %lld, miss: %c\n\n", exec_time, miss);
+
+    p->buffer.last = p->task_struct_pointer->se.sum_exec_runtime;
+  }
+
+  jprobe_return();
+}
+
+static const struct file_operations sched_dl_fops = {
+  .owner = THIS_MODULE,
+  .open =  sched_dl_open,
+  .read = seq_read,
+  .llseek = seq_lseek,
+  .release = single_release,
+};
+
+static void inst_enqueue_task_dl(struct rq * rq,
+                                 struct task_struct * ts,
+                                 int flags)
+{
+  struct task_list_elem_t * p;
+  char path_buffer[50];
+  //char other_buffer[50];
+
+  path_buffer[0] = '\0';
+
+  //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"replenishing PID( %d ) PGRP( %d )\n", ts->pid, ts->tgid);
+
+  p = get_elem_by_task_pid(head, ts->pid);
+  if (!p) {
+    //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"unknown pid found, creating instance\n");
+    p = create_task_list_elem(ts->pid);
+    p->dl_descriptor = &(ts->dl);
+    p->task_struct_pointer = ts;
+
+    if (sched_dl_folder) {
+      // creating process file //
+
+      kitoa(p->pid, path_buffer, 10);
+      p->file = proc_create_data(path_buffer, 0444, sched_dl_folder, &sched_dl_fops, p);
+
+      //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"created file with address: %p\n", p->file);
+      //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"passed parameter: %p\n", p);
+    }
+
+    insert_task_list_elem(&head, p);
+  }
+
+  //printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"enqueuing new value\n");
+
+  jprobe_return();
+}
+
+static struct jprobe enqueue_task_dl_jprobe = {
+  .kp = {
+    .symbol_name	= "enqueue_task_dl",
+  },
+  .entry = (kprobe_opcode_t *) inst_enqueue_task_dl
+};
+
+static struct jprobe replenish_dl_entity_jprobe = {
+  .kp = {
+    .symbol_name	= "update_dl_entity",
+    //.symbol_name	= "replenish_dl_entity",
+  },
+  .entry = (kprobe_opcode_t *) inst_replenish_dl_entity
+};
+
+int init_module(void)
+{
+  head = 0;
+
+  sched_dl_folder = proc_mkdir(PROC_FS_FOLDER_NAME, NULL);
+  if(!sched_dl_folder) {
+    printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"error creating /proc/ folder\n");
+    return -ENOMEM;
+  }
+
+  register_jprobe(&enqueue_task_dl_jprobe);
+
+  printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"plant jprobe at %p, handler addr %p\n",
+         enqueue_task_dl_jprobe.kp.addr, enqueue_task_dl_jprobe.entry);
+
+  register_jprobe(&replenish_dl_entity_jprobe);
+
+  printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"plant jprobe at %p, handler addr %p\n",
+         enqueue_task_dl_jprobe.kp.addr, enqueue_task_dl_jprobe.entry);
+
+  return 0;
+}
+
+void cleanup_module(void)
+{
+  unregister_jprobe(&replenish_dl_entity_jprobe);
+  unregister_jprobe(&enqueue_task_dl_jprobe);
+
+  printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"jprobe unregistered\n");
+
+  //print_statistics(head);
+  clear_task_list(&head);
+
+  remove_proc_entry(PROC_FS_FOLDER_NAME, NULL);
+}
+
+MODULE_AUTHOR("Alessio Balsini");
+MODULE_DESCRIPTION("For each SCHED_DEADLINE scheduled process, a new readable /proc/sched_deadline/[PID] inode will be created, containing tasks' execution information");
+MODULE_LICENSE("GPL");
---
+++ utility.h
@@ -0,0 +1,224 @@
+#ifdef DEBUGGING
+#define pid_t u64
+#define vmalloc malloc
+#define vfree free
+#endif
+#ifdef DEBUGGING_KERNEL_FLAG
+#endif
+
+#define PERIODS_TO_REMEMBER 4000
+
+struct complex_ret_t {
+  int value;
+  void * pointer;
+};
+
+struct circular_buffer_t {
+  int head, tail, count;
+  u64 last;
+  u64 C[PERIODS_TO_REMEMBER];
+  struct timespec time[PERIODS_TO_REMEMBER];
+  char miss[PERIODS_TO_REMEMBER];
+};
+
+struct task_list_elem_t {
+  struct task_list_elem_t * next;
+
+  pid_t pid;
+
+  void * dl_descriptor;
+  struct task_struct * task_struct_pointer;
+  struct proc_dir_entry * file;
+
+  struct circular_buffer_t buffer;
+};
+
+/*
+ * Creates a new task_list_elem and inserts it
+ * in list's head.
+ */
+static struct task_list_elem_t * create_task_list_elem(pid_t pid)
+{
+  //int i;
+  struct task_list_elem_t * t = vmalloc(sizeof(struct task_list_elem_t));
+
+  t->file = NULL;
+
+  t->buffer.head = 0;
+  t->buffer.tail = 0;
+  t->buffer.count = 0;
+
+  t->buffer.last = 0;
+
+  //for (i=0; i<PERIODS_TO_REMEMBER; ++i)
+  //  t->buffer.C[i] = 0;
+
+  t->pid = pid;
+  t->next = 0;
+
+  return t;
+}
+
+/*
+ * Creates a new task_list_elem and inserts it
+ * in list's head.
+ */
+static void insert_task_list_elem(struct task_list_elem_t ** head, struct task_list_elem_t * elem)
+{
+  elem->next = *head;
+  *head = elem;
+}
+
+/*
+ * Removes all the elements from the list
+ */
+static void clear_task_list(struct task_list_elem_t ** head)
+{
+  struct task_list_elem_t * p;
+
+  while (*head) {
+    if ((*head)->file != NULL)
+      proc_remove((*head)->file);
+    p = (*head)->next;
+    vfree(*head);
+    *head = p;
+  }
+}
+/*
+static void print_statistics(struct task_list_elem_t * p)
+{
+  unsigned int i;
+  unsigned int hh;
+
+  while (p) {
+    printk(CONSOLE_LOG_LEVEL MODULE_NAME_PRINTK"Task ( %d ):\n", p->pid);
+    hh = p->buffer.head;
+    for (i=0; i<PERIODS_TO_REMEMBER; i++) {
+      printk(CONSOLE_LOG_LEVEL "  %d : %lld\n", i, p->buffer.C[hh]);
+      hh = (hh + 1) % PERIODS_TO_REMEMBER;
+    }
+    p = p->next;
+  }
+}
+*/
+/*
+ * Returns the pointer of the struct corresponding
+ * to the PID parameter received
+ */
+static struct task_list_elem_t * get_elem_by_task_pid(struct task_list_elem_t * head, pid_t pid)
+{
+  struct task_list_elem_t * p = head;
+
+  while (p && p->pid != pid)
+    p = p->next;
+
+  return p;
+}
+
+/*
+ * Returns the pointer of the struct corresponding
+ * to the dl_descriptor parameter received
+ */
+static struct task_list_elem_t * get_elem_by_task_dl_descriptor(struct task_list_elem_t * head, void * dl_descriptor)
+{
+  struct task_list_elem_t * p = head;
+
+  while (p && p->dl_descriptor != dl_descriptor)
+    p = p->next;
+
+  return p;
+}
+
+/*
+ * Returns the pointer of the struct corresponding
+ * to the file parameter received
+ */
+/*
+static struct task_list_elem_t * get_elem_by_task_file(struct task_list_elem_t * head, struct proc_dir_entry * file)
+{
+  struct task_list_elem_t * p = head;
+
+  while (p && p->file != file)
+    p = p->next;
+
+  return p;
+}
+*/
+
+static u64 cb_dequeue(struct circular_buffer_t * buffer)
+{
+  u64 ret;
+
+  if (buffer->count == 0) {
+    return -1;
+  }
+  ret = buffer->C[buffer->head];
+  buffer->head = (buffer->head + 1) % PERIODS_TO_REMEMBER;
+  buffer->count--;
+
+  return ret;
+}
+
+static void cb_enqueue(struct circular_buffer_t * buffer, u64 v, char miss, struct timespec time)
+{
+  if (buffer->count == PERIODS_TO_REMEMBER)
+    cb_dequeue(buffer);
+  buffer->C[buffer->tail] = v;
+  buffer->miss[buffer->tail] = miss;
+  buffer->time[buffer->tail] = time;
+  buffer->tail = (buffer->tail + 1) % PERIODS_TO_REMEMBER;
+  buffer->count++;
+}
+
+/*
+static u64 cb_get(struct circular_buffer_t * buffer, int i)
+{
+  return buffer->C[(buffer->head + i) % PERIODS_TO_REMEMBER];
+}
+*/
+
+int kstrlen(char * s)
+{
+  int i=0;
+  while (s[i] != '\0') i++;
+  return i;
+}
+
+char * kstrrev(char *str) {
+  char *p1, *p2;
+  if (!str || !*str)
+    return str;
+  for (p1 = str, p2 = str + kstrlen(str) - 1; p2 > p1; ++p1, --p2) {
+    *p1 ^= *p2;
+    *p2 ^= *p1;
+    *p1 ^= *p2;
+  }
+  return str;
+}
+
+void kitoa(int n, char *s, int b) {
+  static char digits[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+  int i=0, sign;
+
+  if ((sign = n) < 0)
+    n = -n;
+  do {
+    s[i++] = digits[n % b];
+  } while ((n /= b) > 0);
+  if (sign < 0)
+    s[i++] = '-';
+  s[i] = '\0';
+  s = kstrrev(s);
+}
+
+void kstrcat(char * dst, char * src)
+{
+  int i = 0;
+  int j = 0;
+  while (dst[i] != '\0')
+    i++;
+  do {
+    dst[i] = src[j];
+    i++;
+  } while (src[j++] != '\0');
+}
